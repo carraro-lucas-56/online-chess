7d3d7867a4321a16a8e4162e10b837e2db232ca3
Lucas Carraro <lucas.carraro555@gmail.com>
2026-02-13 15:53:51 -0300

    added the correct server file

--- server.py
+++ server.py
@@ -2,24 +2,23 @@ import socket
 import pickle
 import random
 import os
-import logging
 from dotenv import load_dotenv
+import logging
 
 from chess.piece import PieceColor, Move
 
-logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-)
+load_dotenv()
 
 logger = logging.getLogger(__name__)
 
-load_dotenv()
-SERVER_IP = os.getenv("SERVER_IP")
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(levelname)s] %(name)s %(message)s")
+
+os.getenv("SERVER_IP")
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-server = SERVER_IP
+server = os.getenv("SERVER_IP")
 port = 5555
 
 try:
@@ -32,16 +31,22 @@ logger.info("Server wating for connections")
 
 while True:
     x = random.randint(1,2)
-    
+
+    p1_color = PieceColor(x)
+    p2_color = PieceColor(1 if x == 2 else 2)
+
     conn, addr = s.accept()
     logger.info(f"connected to {addr}")
-    
-    conn.send(pickle.dumps(PieceColor(x)))
 
+    # conn.send(pickle.dumps(p1_color))
+    
     conn2, addr = s.accept()
     logger.info(f"connected to {addr}")
     
-    conn2.send(pickle.dumps(PieceColor(1 if x == 2 else 2)))
+    # conn2.send(pickle.dumps(p2_color))
+    
+    conn.send(pickle.dumps(p1_color))
+    conn2.send(pickle.dumps(p2_color))
 
     (white,black) = (conn,conn2) if x == 1 else (conn2,conn) 
 
@@ -57,15 +62,13 @@ while True:
         
         except socket.error as e:
             logger.error(f"Game ended, client disconnected")
-
-        except Exception:
-            logger.exception("Unexpected error during game")
-
-        finally:
             white.close()
             black.close()
             break
 
-    
-
+        except Exception:
+            logger.exception("Unexpected error during game")
+            white.close()
+            black.close()
+            break
     
